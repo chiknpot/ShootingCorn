c037e0df1aa85b2d4b66cb16dbf52d71f94c5431
chiknpot <59385390+chiknpot@users.noreply.github.com>
2021-01-31 15:53:07 +0900

    Create val_check.py

---
+++ public/val_check.py
@@ -0,0 +1,8 @@
+epochs = range(1, len(history.history['acc']) + 1)
+plt.plot(epochs, history.history['loss'])
+plt.plot(epochs, history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.show()
